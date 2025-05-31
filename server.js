23a6ce8680df21b5ab6acc8f2a17c06fa3de5095
Chelle77322 <serenynadarra@gmail.com>
2025-05-31 10:21:46 +0930

    Fix of backend

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express, { json } from "express";
 import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.get("/health", (request,result)=>result.send("Server is up and running"));
+app.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
 
 app.use(cors());
 app.use(json());
@@ -9,7 +11,7 @@ app.use(json());
 let trades = [];
 
 app.get("/", (request, result) => {
-  result.send("Satosync Backend is running.");
+  result.send("The Satosync App Server is running.");
 });
 
 app.get("/trades", (request, result) => {
